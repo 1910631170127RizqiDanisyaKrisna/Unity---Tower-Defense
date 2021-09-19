6d4519f5dc5dd2389bd0df4bcbc1d604c2f82bb2
Rizqi Danisya Krisna <83813922+1910631170127RizqiDanisyaKrisna@users.noreply.github.com>
2021-09-19 12:16:23 +0700

    Tugas Programming 4

---
+++ Tower Defense Source/Assets/Script/audioPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioPlayer : MonoBehaviour
+{
+    private static audioPlayer _instance = null;
+
+    public static audioPlayer Instance
+
+    {
+
+        get
+
+        {
+
+            if (_instance == null)
+
+            {
+
+                _instance = FindObjectOfType<audioPlayer> ();
+
+            }
+
+ 
+
+            return _instance;
+
+        }
+
+    }
+
+ 
+
+    [SerializeField] private AudioSource _audioSource;
+
+    [SerializeField] private List<AudioClip> _audioClips;
+
+ 
+
+    public void PlaySFX (string name)
+
+    {
+
+        AudioClip sfx = _audioClips.Find (s => s.name == name);
+
+        if (sfx == null)
+
+        {
+
+            return;
+
+        }
+
+ 
+
+        _audioSource.PlayOneShot (sfx);
+
+    }
+}
